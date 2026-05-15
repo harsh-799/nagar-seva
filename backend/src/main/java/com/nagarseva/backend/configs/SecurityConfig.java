c45a6ecffddd0cb51e148804c65fe89db1c9b0b9
harsh-799 <harshanand7898@gmail.com>
2026-05-15 21:54:47 +0530

    backedn: cors stuffs will be removed by rebasing

--- backend/src/main/java/com/nagarseva/backend/configs/SecurityConfig.java
+++ backend/src/main/java/com/nagarseva/backend/configs/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,6 +13,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @AllArgsConstructor
@@ -33,7 +39,7 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
         );
         http.csrf(csrf -> csrf.disable());
-
+        http.cors(Customizer.withDefaults());
         http.formLogin(form -> form.disable());
         http.httpBasic(basic -> basic.disable());
 
@@ -54,5 +60,43 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration
                                                        config) {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsConfigurationSource
+    corsConfigurationSource() {
+
+        CorsConfiguration configuration =
+                new CorsConfiguration();
+
+        configuration.setAllowedOrigins(
+                List.of("http://localhost:5173")
+        );
+
+        configuration.setAllowedMethods(
+                List.of(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "DELETE",
+                        "OPTIONS"
+                )
+        );
+
+        configuration.setAllowedHeaders(
+                List.of("*")
+        );
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration(
+                "/**",
+                configuration
+        );
+
+        return source;
     }
 }
--- backend/src/main/java/com/nagarseva/backend/controller/AuthController.java
+++ backend/src/main/java/com/nagarseva/backend/controller/AuthController.java
@@ -9,14 +9,12 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
 @Validated
+@CrossOrigin(origins = "http://localhost:5173/")
 public class AuthController {
 
     private AuthService authService;
